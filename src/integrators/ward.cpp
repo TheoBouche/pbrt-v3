4fde9e70afa7d299359b666976f89dcc7b5f09fb
Theo Bouche <to.bouc@gmail.com>
2021-05-10 13:11:38 +0200

    made corrections to the preprocess and added the Li method to WardIntegrator

--- src/integrators/ward.cpp
+++ src/integrators/ward.cpp
@@ -11,20 +11,25 @@ namespace pbrt {
 void WardIntegrator::Preprocess(const Scene &scene,
                                 Sampler &sampler) {
   std::vector<int> nLightSamples;
+  //create a new instance of the sampler for sampling during preprocess
+  std::unique_ptr<Sampler> preSampler = sampler.Clone(0);
+  //initialize the sampler (the point used is not important in this case)
+  Point2i pixel(0.0,0.0);
+  preSampler->StartPixel(pixel);
   
   for (const auto &light : scene.lights) {
-    nLightSamples.push_back(sampler.RoundCount(light->nSamples));
+    nLightSamples.push_back(preSampler->RoundCount(light->nSamples));
   }
   
   // Request samples for sampling all lights
   for (size_t i = 0; i < scene.lights.size(); ++i) {
-    sampler.Request2DArray(nLightSamples[i]);
+    preSampler->Request2DArray(nLightSamples[i]);
   }
-
+  std::cout<<std::endl;
   //sample points on the lights and put the resulting point lights in the lightSources vector to make re-ordering them easier later on
   for (size_t i = 0; i < scene.lights.size(); ++i) {
     int nSamples = nLightSamples[i];
-    const Point2f *uLightArray = sampler.Get2DArray(nSamples);
+    const Point2f *uLightArray = preSampler->Get2DArray(nSamples);
     //if the array wasn't sampled correctly, sample the source normally
     if(!uLightArray){
       //store a point on the source
@@ -52,9 +57,42 @@ void WardIntegrator::Preprocess(const Scene &scene,
 Spectrum WardIntegrator::Li(const RayDifferential &ray,
                                       const Scene &scene, Sampler &sampler,
                                       MemoryArena &arena, int depth) const {
-  return Spectrum(0.0f); //placeholder
-}
+  BxDFType bsdfFlags = BxDFType(BSDF_ALL);
+  Spectrum L(0.f);
+  SurfaceInteraction isect;
+  //if no intersection found, consider that there is no incoming light (not considering environment maps)
+  if (!scene.Intersect(ray, &isect)) {
+    return L;
+  }
 
+  isect.ComputeScatteringFunctions(ray, arena);
+  if (!isect.bsdf){
+    return Li(isect.SpawnRay(ray.d), scene, sampler, arena, depth);
+  }
+  if (lightSources.size() > 0) {
+    for(int i = 0; i<lightSources.size();++i){
+      Vector3f wi;
+      Float lightPdf = 0;
+      VisibilityTester visibility;
+      Spectrum Li = (*lightSources[i].light)->Sample_Li(isect, lightSources[i].uLight, &wi, &lightPdf, &visibility); //TODO : try to work out a more elegant way to work with Shared_ptr
+      if (lightPdf > 0 && !Li.IsBlack()) {
+	//compute the BSDF
+	Spectrum f;
+	f = isect.bsdf->f(isect.wo, wi, bsdfFlags) * AbsDot(wi, isect.shading.n);
+	if (!f.IsBlack()) {
+	  if (!visibility.Unoccluded(scene)) {
+	    Li = Spectrum(0.f);
+	  }
+	  
+	  if (!Li.IsBlack()) {
+	    L += f * Li / lightPdf;
+	  }
+	}
+      }
+    } 
+  }
+  return L;
+}
   
 WardIntegrator *CreateWardIntegrator(
     const ParamSet &params, std::shared_ptr<Sampler> sampler,
